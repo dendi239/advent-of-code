b11fab641b29304c4e07f61c622171d262653117
Denys Smirnov <dendi239@gmail.com>
2023-12-09 18:48:59 +0000

    add day 07

---
+++ 07-camel-cards/part-two.py
@@ -0,0 +1,69 @@
+import argparse
+import os
+import itertools
+
+
+BY_STRENGTH = ["A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"]
+POWER = {k: f"{i:02}" for i, k in enumerate(reversed(BY_STRENGTH))}
+
+
+def the_same(s: str) -> bool:
+    return s[:-1] == s[1:]
+
+
+def canonical_hand(hand: str) -> str:
+    power = "".join(POWER[c] for c in hand)
+    hand = "".join(reversed(sorted(hand, key=lambda x: POWER[x])))
+    if the_same(hand):
+        return f"7{power}"
+    if the_same(hand[:-1]) or the_same(hand[1:]):
+        return f"6{power}"
+    if (the_same(hand[:2]) and the_same(hand[2:])) or (
+        the_same(hand[:3]) and the_same(hand[3:])
+    ):
+        return f"5{power}"
+    if the_same(hand[:3]) or the_same(hand[1:4]) or the_same(hand[2:]):
+        return f"4{power}"
+    if (the_same(hand[:2]) and (the_same(hand[2:4]) or the_same(hand[3:]))) or (
+        the_same(hand[1:3]) and the_same(hand[3:])
+    ):
+        return f"3{power}"
+    if any(a == b for a, b in zip(hand, hand[1:])):
+        return f"2{power}"
+    return f"1{power}"
+
+
+def canonical_hand2(hand: str) -> str:
+    return canonical_hand(pick_jockers(hand))[:1] + canonical_hand(hand)[1:]
+
+
+def pick_jockers(hand: str) -> str:
+    indices = [i for i in range(len(hand)) if hand[i] == "J"]
+
+    def replace(replacements):
+        chars = list(hand)
+        for i, r in zip(indices, replacements):
+            chars[i] = r
+        res = "".join(chars)
+        return res
+
+    return max(
+        map(
+            replace, itertools.combinations_with_replacement(BY_STRENGTH, len(indices))
+        ),
+        key=canonical_hand,
+    )
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", default=f"{os.path.dirname(__file__)}/input.txt")
+    args = parser.parse_args()
+    with open(args.input) as f:
+        hand_bids = [(ts[0], int(ts[1])) for line in f if (ts := line.split())]
+        hand_bids = sorted(hand_bids, key=lambda x: canonical_hand2(x[0]))
+        print(sum((i + 1) * b for i, (_, b) in enumerate(hand_bids)))
+
+
+if __name__ == "__main__":
+    main()
