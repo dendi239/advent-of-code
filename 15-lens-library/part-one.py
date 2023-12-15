e8b2d4f8e7a779fe8a100335d30acb50bad451b7
Denys Smirnov <dendi239@gmail.com>
2023-12-15 22:06:22 +0100

    15 lens library: simple managing complex state

---
+++ 15-lens-library/part-one.py
@@ -0,0 +1,18 @@
+def Hash(s: str) -> int:
+    res = 0
+    for c in s:
+        res += ord(c)
+        res *= 17
+        res %= 256
+    return res
+
+
+def main() -> None:
+    with open("15-lens-library/input.txt") as f:
+        l = f.read()
+
+    print(sum(Hash(s) for st in l.split(",") if (s := st.strip())))
+
+
+if __name__ == "__main__":
+    main()
