5ae9be76462555dd262a7e5b8ee14e4665bd72ff
Denys Smirnov <dendi239@gmail.com>
2024-12-07 13:31:54 +0000

    Repair some bridges

---
+++ 2024-07-bridge-repair/part-one.py
@@ -0,0 +1,25 @@
+def reachable(xs: list[int]) -> set[int]:
+    results = {xs[0]}
+    for x in xs[1:]:
+        next_ = {x + y for y in results} | {x * y for y in results}
+        results = next_
+    return results
+
+
+def main() -> None:
+    with open("2024-07-bridge-repair/input.txt") as f:
+        data = {
+            int(lhs): [int(x) for x in rhs.split()]
+            for line in f
+            if line
+            if (parts := line.strip().split(":"))[0]
+            if (lhs := parts[0]) and (rhs := parts[1])
+        }
+
+    print(
+        sum(target for target, numbers in data.items() if target in reachable(numbers))
+    )
+
+
+if __name__ == "__main__":
+    main()
